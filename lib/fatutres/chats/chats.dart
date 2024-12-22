b68dae2ab765c1f76fe77f71954bc7ac373ad345
Korya25 <enthrj3@gmail.com>
2024-12-22 16:08:02 +0200

    Initial commit

---
+++ lib/fatutres/chats/chats.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/fatutres/chats/widgets/chat_app_bar.dart';
+
+class Chats extends StatelessWidget {
+  const Chats({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: ChatsAppBar(),
+    );
+  }
+}
